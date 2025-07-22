9812619a19c857e9aa148f0637d4a9b7e36379da
Max112.87 <max112.87>
2025-07-22 20:29:05 +0700

    добавлен тест "Незнайка и его друзья" из локального файла

--- src/app/data/answers.ts
+++ src/app/data/answers.ts
@@ -1,15 +1,52 @@
 export const correctAnswers = [
 	{
 		id: 1,
+
 		correctOptions: ['b'],
 	},
 	{
 		id: 2,
-		correctOptions: ['b', 'c'], // если multiple, то можно например ['b', 'c']
+
+		correctOptions: ['a'],
 	},
 	{
 		id: 3,
-		correctOptions: ['b', 'c'], // если multiple, то можно например ['b', 'c']
+
+		correctOptions: ['a'],
 	},
-	// другие вопросы...
+	{
+		id: 4,
+
+		correctOptions: ['b'],
+	},
+	{
+		id: 5,
+
+		correctOptions: ['c'],
+	},
+	{
+		id: 6,
+
+		correctOptions: ['a'],
+	},
+	{
+		id: 7,
+
+		correctOptions: ['a'],
+	},
+	{
+		id: 8,
+
+		correctOptions: ['a', 'c'],
+	},
+	{
+		id: 9,
+
+		correctOptions: ['b'],
+	},
+	{
+		id: 10,
+
+		correctOptions: ['a', 'b'],
+	},
 ];
